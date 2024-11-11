fdea5e0f827ef7d1cace95c92164dbd3605490d7
Nehemy <nehemynet@gmail.com>
2024-11-11 16:02:10 +0100

    First commit

---
+++ Computer Science Quiz Game/data.py
@@ -0,0 +1,153 @@
+question_data = [
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "RAM stands for Random Access Memory.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science: Computers",
+                "question": "AMD created the first consumer 64-bit processor.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science: Computers",
+                "question": "The common software-programming acronym &quot;I18N&quot; comes from the term &quot;Interlocalization&quot;.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science: Computers",
+                "question": "To bypass US Munitions Export Laws, the creator of the PGP published all the source code in book form. ",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science: Computers",
+                "question": "The very first recorded computer &quot;bug&quot; was a moth found inside a Harvard Mark II computer.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science: Computers",
+                "question": "The first computer bug was formed by faulty wires.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "hard",
+                "category": "Science: Computers",
+                "question": "The T-Mobile Sidekick smartphone is a re-branded version of the Danger Hiptop.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science: Computers",
+                "question": "Android versions are named in alphabetical order.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science: Computers",
+                "question": "A Boolean value of &quot;0&quot; represents which of these words?",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "hard",
+                "category": "Science: Computers",
+                "question": "DHCP stands for Dynamic Host Configuration Port.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science: Computers",
+                "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "hard",
+                "category": "Science: Computers",
+                "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "The Windows ME operating system was released in the year 2000.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            }
+]
+
