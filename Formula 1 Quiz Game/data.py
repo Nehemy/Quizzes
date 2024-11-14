491bed64586c0d1101492a8efc90ac8dbf926557
Nehemy <nehemynet@gmail.com>
2024-11-14 18:40:02 +0100

    F1 questions added

---
+++ Formula 1 Quiz Game/data.py
@@ -0,0 +1,7 @@
+question_data = [
+    {"question": "Lewis Hamilton has won 7 World Drivers' Championships.", "correct_answer": "True"},
+    {"question": "Michael Schumacher holds the record for the most Formula 1 wins.", "correct_answer": "False"},
+    {"question": "Sebastian Vettel won his first championship in 2010.", "correct_answer": "True"},
+    {"question": "Ferrari has the most Constructors' Championships.", "correct_answer": "True"},
+    {"question": "A Formula 1 race typically lasts for 200 laps.", "correct_answer": "False"},
+]
